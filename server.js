1b7d33b5b30cc9c5b8ee2d8e6a2cc8efc4ff3313
unknown <jussi.kaatiala@gmail.com>
2019-12-30 15:15:26 +0200

    Added some comments on code

--- server.js
+++ server.js
@@ -14,15 +14,20 @@ var index_view = fs.readFileSync(indexfile, "utf-8");
 var dbname = "./db/meerkat.db";
 var cookie_ttl = 60*60; // seconds: 60*60*24 is one day
 
-var portnum = 80;
+// accept cli arguments and remove unnecessary
 var arguments = process.argv.slice(2);
 
+// use cli arguments to decide listening port
+var portnum = 80;
 if (arguments.length > 1) {
+   // too much arguments
    console.error("Maximum argument count 1: port for server to listen");
    return;
 } else if (arguments.length == 1 ) {
+   // right amount of arguments
    var portnum = Number(arguments[0]);
    if (isNaN(portnum)) {
+      // make sure argument was a proper number
       console.error("Expecting a numerical argument.");
       return;
    }
