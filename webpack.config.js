c40ad4442c1ddc4bda124afc5a3c88ae441f61b6
unknown <jussi.kaatiala@gmail.com>
2020-03-10 23:22:31 +0200

    Added css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
             loader: "html-loader"
           }
         ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       }]
   },
   output: {
