85fb38ca9d70236233fef5ebd9777c5b345afc43
unknown <jussi.kaatiala@gmail.com>
2020-03-10 17:49:01 +0200

    Build now renders on browser

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
 // a node.js webserver without any frameworks
 // run: "node ./server"
 
-const fs = require("fs")
-const ejs = require("ejs")
-const sqlite3 = require("sqlite3").verbose()
-const qs = require("querystring")
-const crypto = require("crypto")
-const dbpath = require("./dbpath")
 const express = require('express')
 
-
-
-// load the base-page template to RAM
-var indexfile = "views/index.ejs";
-var index_view = fs.readFileSync(indexfile, "utf-8");
 var cookie_ttl = 60*60; // seconds: 60*60*24 is one day
 
 // accept cli arguments and remove unnecessary
@@ -38,22 +27,9 @@ if (arguments.length > 1) {
 // start node express
 const app = express()
 
-app.use('/css', express.static(__dirname + '/css'));
-
-app.get('/', on_get)
-
-app.post('/', on_post)
-
-app.post('/notes/', on_note_post)
+app.use(express.static('build'))
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-})
-
-var db = new sqlite3.Database(dbpath, (err) => {
-  // connect to database and start server
-  if (err) {
-    return console.error(err.message);
-  }
 })
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App = () => {
-  return (<h1>moi</h1>)
+  return (<h1>Meerkat</h1>)
 }
 
 export default App
