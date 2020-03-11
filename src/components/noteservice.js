6066b082d9ad98eb39cd8337748303c7ded32f1f
unknown <jussi.kaatiala@gmail.com>
2020-03-11 22:52:11 +0200

    Added del and update to noteservice

--- src/components/noteservice.js
+++ src/components/noteservice.js
@@ -11,5 +11,16 @@ const post = (newentry) => {
   return promise.then((response)=> response.data)
 }
 
+const del = (id) => {
+  const promise = axios.delete(`${baseUrl}/${id}`)
+  return promise.then((response) => {
+    return response.data
+  }) 
+}
 
-export default {getAll, post}
+const update = (id, newObject) => {
+  const request = axios.put(`${baseurl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+export default {getAll, post, del, update}
