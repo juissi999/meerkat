58ee86f4e68e958ee401d2680783099ac46a665c
unknown <jussi.kaatiala@gmail.com>
2020-03-15 17:59:07 +0200

    hashtags processing moved to client

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,42 @@ import noteservice from './noteservice'
 
 let notificationTimer = null
 
+const findHashtags = (notestr) => {
+  let hashtags = [];
+
+  // remove all control characters (line endings etc)
+  notestr = notestr.replace(/[^\x20-\x7E]/gmi, " ")
+
+  notestr.split(" ").map((element) => {
+     if (element[0] == "#" && element.length > 1) {
+        // trim all whitespaces and newlines from the hashtag string
+        const trimmed_el = element.trim()
+
+        // if hashtag not present yet, add it
+        if (hashtags.indexOf(trimmed_el) < 0) {
+           hashtags.push(trimmed_el)
+        }
+
+     }
+  })
+
+  return hashtags
+}
+
+const updateHashtags = (data, setHashtags) => {
+  const htarray = data.reduce((accumulator, currentdata) => {
+    const ht = findHashtags(currentdata.text)
+    const htmap = ht.map((h) => {
+      return {name:h, linksto:currentdata.noteid} // [{name:'test', selected:true}]
+    })
+
+    return accumulator.concat(htmap)
+  }, [])
+  console.log(htarray)
+
+  setHashtags(htarray)
+}
+
 const Notification = ({msg, setNotification}) => {
 
   clearTimeout(notificationTimer)
@@ -18,18 +54,22 @@ const Notification = ({msg, setNotification}) => {
   return (<div className='notification'>{msg}</div>)
 }
 
-const Memory = ({note, notes, setNotes, setNotification}) => {
-  
+const Memory = ({note, notes, setNotes, setNotification, setHashtags}) => {
+
   const datestr = new Date(note.date).toString()
 
   const onClickDelete = () => {
     noteservice.del(note.noteid)
-      .then(setNotes(notes.filter(n => {
-        if (n.noteid !== note.noteid) {
-          return n
-        }
+      .then(() => {
+        const newNotes = notes.filter(n => {
+          if (n.noteid !== note.noteid) {
+            return n
+          }  
+        })
+        setNotes(newNotes)
+        updateHashtags(newNotes, setHashtags)
         setNotification(`Note deleted.`)
-      })))
+      })
   }
 
   return (
@@ -43,9 +83,9 @@ const Memory = ({note, notes, setNotes, setNotification}) => {
     </div>)
 }
 
-const Memories = ({notes, setNotes, setNotification}) => {
+const Memories = ({notes, setNotes, setNotification, setHashtags}) => {
   return (<div className={'memory_container'}>
-  {notes.map((note)=> {return <Memory key={notes.noteid} note={note} notes={notes} setNotes={setNotes} setNotification={setNotification}/>})}
+  {notes.map((note)=> {return <Memory key={note.noteid} note={note} notes={notes} setNotes={setNotes} setNotification={setNotification} setHashtags={setHashtags}/>})}
   </div>
   )
 }
@@ -58,12 +98,12 @@ const Hahstagbutton = ({name, selected}) => {
  }
 }
 
-const Hashtags = ({hashtags}) => {
+const Hashtags = ({hashtags, selected}) => {
   return (<div className='hashtags_container'>
   <form name = 'hashtagsform' action='/' method='POST'>
     <button name='selected_hashtag' value='all' type='submit' className='hashtag'>Show all</button>
     {hashtags.map((ht, i) => {
-      <Hahstagbutton key={i} name={ht.name} selected={ht.selected}/>
+      return (<Hahstagbutton key={i} name={ht.name} selected={selected}/>)
     })}
   </form>
   </div>)
@@ -76,7 +116,7 @@ const Logoutform = ({username}) => {
   </form>)
 } 
 
-const Pushform = ({memo, setMemo, notes, setNotes, setNotification}) => {
+const Pushform = ({memo, setMemo, notes, setNotes, setNotification, setHashtags}) => {
 
   const newentry = {'text':memo}
 
@@ -86,7 +126,9 @@ const Pushform = ({memo, setMemo, notes, setNotes, setNotification}) => {
     noteservice
       .post(newentry)
       .then(addedentry => {
-        setNotes(notes.concat(addedentry))
+        const newNotes = notes.concat(addedentry)
+        setNotes(newNotes)
+        updateHashtags(newNotes, setHashtags)
         setMemo('')
         setNotification('New entry added!')
     })
@@ -108,10 +150,15 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [memo, setMemo] = useState('')
   const [notification, setNotification] = useState(null)
+  const [hashtags, setHashtags] = useState([])
   
   const getAll = () => {
     noteservice.getAll()
-      .then(data => setNotes(data))
+      .then(data => {
+
+        updateHashtags(data, setHashtags)
+        setNotes(data)
+      })
   }
   
   useEffect(getAll, [])
@@ -119,9 +166,9 @@ const App = () => {
   return (<><h1>Meerkat</h1>
           <Notification msg={notification} setNotification={setNotification}/>
           <Logoutform username={'test'}/>
-          <Pushform notes={notes} setNotes={setNotes} memo={memo} setMemo={setMemo} setNotification={setNotification}/>
-          <Hashtags hashtags={[{name:'test', selected:true}]}/>
-          <Memories notes={notes} setNotes={setNotes} setNotification={setNotification}/>
+          <Pushform notes={notes} setNotes={setNotes} memo={memo} setMemo={setMemo} setNotification={setNotification} setHashtags={setHashtags}/>
+          <Hashtags hashtags={hashtags} selected={false}/>
+          <Memories notes={notes} setNotes={setNotes} setNotification={setNotification} setHashtags={setHashtags}/>
           </>)
 }
 
