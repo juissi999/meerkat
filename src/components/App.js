68850533427d6464437d6e4c6d43081a063a3939
juissi999 <jussi.kaatiala@gmail.com>
2020-04-01 01:35:14 +0300

    Hashtags filtering works now

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ const App = () => {
   const [notification, setNotification] = useState(null)
   const [hashtags, setHashtags] = useState([])
   const [selectedHts, setSelectedHts] = useState([])
+  const [notesShow, setNotesShow] = useState([])
   
   const getAll = () => {
     noteservice.getAll()
@@ -27,13 +28,35 @@ const App = () => {
   // effect-hook updates hashtags every time notes change
   useEffect(() => updateHashtags(notes, setHashtags), [notes])
 
+  // effect-hook filters shown notes every time selected hashtags change
+  // or hashtags are updated (new note, modification etc)
+  useEffect(() => {
+    const sn = notes.filter((n) => {
+
+      // find hastags for this note
+      const hts = hashtags.filter(ht=>ht.linksto===n.noteid)
+
+      // get the noteid from the raw hashtags (technical function)
+      const htNames = hts.map((ht)=>ht.name)
+
+      // go through selected hts and collect matching to found hts
+      const foundHts = selectedHts.filter(sHt=>{
+        return htNames.includes(sHt)
+      })
+
+      return selectedHts.length === foundHts.length
+    })
+
+    setNotesShow(sn)
+  }, [selectedHts, hashtags])
+
   return (<>
             <div id={'headline'}><h1>Meerkat</h1>
               <Notification msg={notification} setNotification={setNotification}/>
             </div>
             <MemoryPushForm notes={notes} setNotes={setNotes} setNotification={setNotification}/>
             <HashtagList hashtags={hashtags} selectedHts={selectedHts} setSelectedHts={setSelectedHts}/>
-            <MemoryList notes={notes} setNotes={setNotes} setNotification={setNotification}/>
+            <MemoryList notes={notesShow} setNotes={setNotes} setNotification={setNotification}/>
           </>)
 }
 
