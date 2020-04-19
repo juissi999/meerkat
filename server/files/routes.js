bb900d3ac7de0323bb8934e2b42dd22f67dc17e3
juissi999 <jussi.kaatiala@gmail.com>
2020-04-19 23:04:18 +0300

    Changed file upload path to builddir

--- server/files/routes.js
+++ server/files/routes.js
@@ -1,14 +1,27 @@
 const router = require('express').Router()
 const multer = require('multer')
 const handlers = require('./handlers')
+const path = require('path')
+const fs = require('fs')
+
+// use webpack config file to get build directory
+const wpconf = require('../../webpack.config')
+const builddir = wpconf.output.path
 
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, 'server/db/uploads/')
+
+    // check if upload directory exists and if not, create it
+    const uploadpath = path.resolve(builddir, 'uploads/')
+    if (!fs.existsSync(uploadpath)) {
+      fs.mkdirSync(uploadpath, { recursive: true })
+    }
+
+    cb(null, uploadpath)
   },
   filename: function(req, file, cb) {
     const timestamp = Math.round((new Date()).getTime() / 1000)
-    cb(null, timestamp + file.originalname)
+    cb(null, timestamp.toString() + '_' + file.originalname)
   }
 })
 
