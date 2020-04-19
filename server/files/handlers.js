5b0b42a6050725e4388377954fff60d4bd9b626b
juissi999 <jussi.kaatiala@gmail.com>
2020-04-20 00:18:18 +0300

    Moved upload-middleware to handlers

--- server/files/handlers.js
+++ server/files/handlers.js
@@ -1,12 +1,42 @@
-
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
 const dbservice = require('../dbservice')
+
+// use webpack config file to get build directory
+const wpconf = require('../../webpack.config')
+const builddir = wpconf.output.path
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+
+    // check if upload directory exists and if not, create it
+    const uploadpath = path.resolve(builddir, 'uploads/')
+    if (!fs.existsSync(uploadpath)) {
+      fs.mkdirSync(uploadpath, { recursive: true })
+    }
+
+    cb(null, uploadpath)
+  },
+  filename: function(req, file, cb) {
+    const timestamp = Math.round((new Date()).getTime() / 1000)
+    cb(null, timestamp.toString() + '_' + file.originalname)
+  }
+})
 
 exports.get = (request, response) => {
 }
 
 exports.post = (request, response) => {
-  console.log(request.file)
-  response.status(200).end()
+
+  const upload = multer({storage:storage}).single('memFile')
+
+  upload(request, response, (err)=> {
+    if (err) {
+      return response.status(status.BAD_REQUEST)
+    } 
+    response.status(200).end()
+  })
 }
 
 exports.delete = (request, response) => {
--- server/files/routes.js
+++ server/files/routes.js
@@ -1,33 +1,7 @@
 const router = require('express').Router()
-const multer = require('multer')
 const handlers = require('./handlers')
-const path = require('path')
-const fs = require('fs')
 
-// use webpack config file to get build directory
-const wpconf = require('../../webpack.config')
-const builddir = wpconf.output.path
-
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-
-    // check if upload directory exists and if not, create it
-    const uploadpath = path.resolve(builddir, 'uploads/')
-    if (!fs.existsSync(uploadpath)) {
-      fs.mkdirSync(uploadpath, { recursive: true })
-    }
-
-    cb(null, uploadpath)
-  },
-  filename: function(req, file, cb) {
-    const timestamp = Math.round((new Date()).getTime() / 1000)
-    cb(null, timestamp.toString() + '_' + file.originalname)
-  }
-})
-
-const upload = multer({storage:storage})
-
-router.post('/', upload.single('memFile'), handlers.post)
+router.post('/', handlers.post)
 
 router.get('/:id', handlers.get)
 
