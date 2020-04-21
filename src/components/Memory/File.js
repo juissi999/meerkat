48e7a73883b4528c67dc99746d14ef898abbd88d
juissi999 <jussi.kaatiala@gmail.com>
2020-04-21 22:38:20 +0300

    File now displays image on memory if jpg or png

--- src/components/Memory/File.js
+++ src/components/Memory/File.js
@@ -3,8 +3,22 @@ import React from 'react'
 const uploaddir = 'uploads'
 
 const MemoryFile = ({children}) => {
-  return (<div>
-            {children.map((c,i)=><a href={'uploads/' + c} key={i}>{c}</a>)}
+
+  const filedisplay = (c,i) => {
+    const filepath = 'uploads/' + c
+    const splitted = filepath.split('.')
+    const extension = splitted[splitted.length-1].toLowerCase()
+    console.log(extension)
+    //<img src={filepath}></img>
+
+    if (extension === 'jpg' || extension === 'png') {
+      return (<a href={filepath} key={i}><img src={filepath} className='memoryimage'></img></a>)
+    }
+    return (<a href={filepath} key={i}>{c}</a>)
+  }
+
+  return (<div className='memfilecontainer'>
+            {children.map(filedisplay)}
           </div>)
 }
 
