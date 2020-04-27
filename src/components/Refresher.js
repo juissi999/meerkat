254df2c74bc39d917c0821fdfeb09d8045e13f66
juissi999 <jussi.kaatiala@gmail.com>
2020-04-27 10:08:01 +0300

    Refresher initial state now separated from code

--- src/components/Refresher.js
+++ src/components/Refresher.js
@@ -2,10 +2,11 @@ import React, {useState, useEffect} from 'react'
 import Form from 'react-bootstrap/Form'
 
 const INTERVAL = 5000
+const INITIAL_STATE = true
 
 const Refresher = ({getterFcn}) => {
 
-  const [checked, setChecked] = useState(false)
+  const [checked, setChecked] = useState(INITIAL_STATE)
 
   useEffect(() => {
     if (checked) {
@@ -21,6 +22,7 @@ const Refresher = ({getterFcn}) => {
   return (<Form>
             <Form.Check 
               onChange={onChange}
+              checked={checked}
               type='checkbox'
               id='refreshCheck'
               label='Automatic refresh'
