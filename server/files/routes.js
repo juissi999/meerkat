11b111549071d959114483b9d4b47e16df9ec934
juissi999 <jussi.kaatiala@gmail.com>
2020-04-29 00:28:21 +0300

    Some API properties commented on routes

--- server/files/routes.js
+++ server/files/routes.js
@@ -1,10 +1,15 @@
 const router = require('express').Router()
 const handlers = require('./handlers')
 
+// SAFE = action does not change the servers state or database
+// IDEMPONENT = effect is the same if you do the action once or n times
+
 router.post('/', handlers.post)
 
+// SAFE, IDEMPONENT
 router.get('/', handlers.getAll)
 
+// SAFE, IDEMPONENT
 router.get('/:id', handlers.get)
 
 module.exports = router
--- server/notes/routes.js
+++ server/notes/routes.js
@@ -1,14 +1,21 @@
 const router = require('express').Router()
 const handlers = require('./handlers')
 
+// SAFE = action does not change the servers state or database
+// IDEMPONENT = effect is the same if you do the action once or n times
+
+// SAFE, IDEMPONENT
 router.get('/', handlers.getAll)
 
 router.post('/', handlers.post)
 
+// SAFE, IDEMPONENT
 router.get('/:id', handlers.getOne)
 
+// IDEMPONENT
 router.delete('/:id', handlers.delete)
 
+// IDEMPONENT
 router.put('/:id', handlers.put)
 
 module.exports = router
