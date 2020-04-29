28d5afa8b10c93ef7308fed1003736407155b8f6
juissi999 <jussi.kaatiala@gmail.com>
2020-04-29 15:30:05 +0300

    Note sorting function added

--- src/components/Note/List.js
+++ src/components/Note/List.js
@@ -3,14 +3,19 @@ import Note from './index'
 
 const NoteList = ({notes, setNotes, notesVisible, setNotification}) => {
 
-  const mapMemories = () => {
-    // make a copy of the array to not reverse original
-    return notesVisible.slice(0).reverse().map((note, i) =>
+  const mapNotes = (notelist) => {
+    // sort notes, sort process can be customized now
+    let sortedNotes = notelist.slice(0)
+    sortedNotes.sort((a,b) => {
+      return b.date - a.date
+    })
+
+    return sortedNotes.map((note, i) =>
       <Note key={i} note={note} notes={notes} setNotes={setNotes} setNotification={setNotification}/>)
   }
 
   return (<>
-            {mapMemories()}
+            {mapNotes(notesVisible)}
           </>)
 }
 
