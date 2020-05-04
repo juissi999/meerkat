fe20c87e340d09ff72c170ece5be6c8526d13183
juissi999 <jussi.kaatiala@gmail.com>
2020-05-04 23:06:50 +0300

    Added mongodb schemas for note and file

---
+++ server/models/file.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+// get environmental variables
+require('dotenv').config()
+
+const url = process.env.MONGODB_URI
+
+console.log('Connecting to', url)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
+    console.log('Connected to MongoDB')  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message)
+  })
+
+const fileSchema = new mongoose.Schema({
+  filename: String,
+  noteid: String
+})
+
+module.exports = mongoose.model('File', fileSchema)
