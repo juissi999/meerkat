3b7d2a0d6b21eca7646822ead5f8076e651f7d74
juissi999 <jussi.kaatiala@gmail.com>
2020-10-21 00:47:18 +0300

    Add a lander with login details

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,15 +4,37 @@ import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
 
 const Landing = () => {
 
   const dispatch = useDispatch()
 
+  const onSubmit = (e) => {
+    e.preventDefault()
+    dispatch({type: 'LOGIN'})
+  }
+
   return (<Container>
             <Row>
               <Col>
-                <Button onClick={()=>dispatch({type: 'LOGIN'})}>Please login here</Button>
+              <h1>meerkat</h1>
+              <Form onSubmit={onSubmit}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Login
+                </Button>
+              </Form>
               </Col>
             </Row>
           </Container>
