a7238005327f584ee0e72219566d7490ee61db0a
juissi999 <jussi.kaatiala@gmail.com>
2020-10-21 19:16:01 +0300

    Create auth service

---
+++ src/services/authService.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+const baseUrl = '/user'
+
+const login = (credentials) => {
+  const request = axios.post(baseUrl, credentials)
+  return request.then((response) => response.data)
+}
+
+const newUser = (newentry) => {
+  const request = axios.post(baseUrl, newentry)
+  return request.then((response) => response.data)
+}
+
+const del = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then((response) => {
+    return response.data
+  })
+}
+
+
+export default { login, newUser, del }
