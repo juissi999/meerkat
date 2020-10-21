ecc61e7f58ef730f58a5265974ea51f3e7315e5e
juissi999 <jussi.kaatiala@gmail.com>
2020-10-21 19:58:01 +0300

    Add state hooks to login fields

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
@@ -7,38 +7,61 @@ import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
 
 const Landing = () => {
-
   const dispatch = useDispatch()
+
+  const [email, setEmail] = useState('')
+  const [passwd, setPasswd] = useState('')
 
   const onSubmit = (e) => {
     e.preventDefault()
-    dispatch({type: 'LOGIN'})
+    const loginCredentials = { email, passwd }
+    console.log(loginCredentials)
+    dispatch({ type: 'LOGIN', data: loginCredentials })
   }
 
-  return (<Container>
-            <Row>
-              <Col>
-              <h1>meerkat</h1>
-              <Form onSubmit={onSubmit}>
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                  Login
-                </Button>
-              </Form>
-              </Col>
-            </Row>
-          </Container>
-         )
+  const onChangePassword = (event) => {
+    setPasswd(event.target.value)
+  }
+
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value)
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>meerkat</h1>
+          <Form onSubmit={onSubmit}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={onChangeEmail}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={passwd}
+                onChange={onChangePassword}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default Landing
