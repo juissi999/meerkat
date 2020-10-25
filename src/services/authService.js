eac1533ddf498b1e4c81651dc0870f99650cd1e6
juissi999 <jussi.kaatiala@gmail.com>
2020-10-25 18:04:50 +0200

    Update service to point to new routes

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,18 +1,18 @@
 import axios from 'axios'
-const baseUrl = '/user'
+const baseUrl = '/auth'
 
 const login = (credentials) => {
-  const request = axios.post('/auth/login', credentials)
+  const request = axios.post('/login', credentials)
   return request.then((response) => response.data)
 }
 
 const newUser = (newentry) => {
-  const request = axios.post(baseUrl, newentry)
+  const request = axios.post(`${baseUrl}/add`, newentry)
   return request.then((response) => response.data)
 }
 
 const del = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`)
+  const request = axios.delete(`${baseUrl}/delete/${id}`)
   return request.then((response) => {
     return response.data
   })
