6c888485ee53db16e0acec6bd586a42b4261e7d4
juissi999 <jussi.kaatiala@gmail.com>
2020-11-01 21:18:45 +0200

    Add reload after login to load token to api

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,6 +17,8 @@ const Main = () => {
 
   const render = () => {
     if (isLoggedIn) {
+      // reload here to get the api use newly saved token
+      window.location.reload()
       return <Meerkat />
     } else {
       return <Landing />
