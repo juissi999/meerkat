d58b2a3f173efdbb5fc4ffd945b37ad240216ea4
juissi999 <jussi.kaatiala@gmail.com>
2020-11-01 22:29:04 +0200

    Add tokenValidator to file-routes too

--- server/files/routes.js
+++ server/files/routes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router()
 const handlers = require('./handlers')
+const tokenValidator = require('../tokenValidator')
+
+// add tokenValidator middleware that validates JWT token and adds a userId
+// field to request
+router.use(tokenValidator({ SECRET: process.env.SECRET }))
 
 // SAFE = action does not change the servers state or database
 // IDEMPONENT = effect is the same if you do the action once or n times
