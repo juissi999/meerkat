1ad319d6a3a985e07fa20a3504a0df526a8278e1
juissi999 <jussi.kaatiala@gmail.com>
2020-11-01 22:29:26 +0200

    Add comments for middleware

--- server/notes/routes.js
+++ server/notes/routes.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 const handlers = require('./handlers')
 const tokenValidator = require('../tokenValidator')
 
+// add tokenValidator middleware that validates JWT token and adds a userId
+// field to request
 router.use(tokenValidator({ SECRET: process.env.SECRET }))
 
 // SAFE = action does not change the servers state or database
