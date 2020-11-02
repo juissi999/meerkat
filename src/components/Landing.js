f29307a7976333c3ae74a17aa43bb9c205d1d27c
juissi999 <jussi.kaatiala@gmail.com>
2020-11-02 22:59:47 +0200

    Add a toggle for showing registration form

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,33 +6,12 @@ import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Form from 'react-bootstrap/Form'
 
-import api from '../services/api'
 import authService from './../services/authService'
 
 const Landing = () => {
-  const dispatch = useDispatch()
-
   const [email, setEmail] = useState('')
   const [passwd, setPasswd] = useState('')
-  const [formAction, setFormAction] = useState('login')
-
-  const onLogin = (e) => {
-    e.preventDefault()
-    const loginCredentials = { email, passwd }
-    const response = authService.login(loginCredentials)
-    //const response = authService.newUser(loginCredentials)
-    response
-      .then((data) => {
-        const token = data.token
-        localStorage.setItem('token', token)
-        // set token to api instance (it was already started)
-        api.defaults.headers.Authorization = `Bearer ${token}`
-        dispatch({ type: 'LOGIN' })
-        // reload here to get the api use newly saved token
-        //window.location.reload()
-      })
-      .catch((err) => console.log(err))
-  }
+  const [showForm, setShowForm] = useState(false)
 
   const onRegister = (e) => {
     e.preventDefault()
@@ -53,45 +32,45 @@ const Landing = () => {
     setEmail(event.target.value)
   }
 
+  const renderRegisterForm = () => {
+    if (showForm) {
+      return (
+        <Form onSubmit={onRegister}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={onChangeEmail}
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={passwd}
+              onChange={onChangePassword}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      )
+    }
+  }
+
   return (
     <Container>
       <Row>
         <Col>
           <h1>meerkat</h1>
-          <Button
-            variant="primary"
-            onClick={() =>
-              setFormAction(formAction === 'login' ? 'register' : 'login')
-            }
-          >
-            {formAction}
+          <Button variant="primary" onClick={() => setShowForm(!showForm)}>
+            Register
           </Button>
-          <Form onSubmit={formAction === 'login' ? onLogin : onRegister}>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                value={email}
-                onChange={onChangeEmail}
-              />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                value={passwd}
-                onChange={onChangePassword}
-              />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+          {renderRegisterForm()}
         </Col>
       </Row>
     </Container>
